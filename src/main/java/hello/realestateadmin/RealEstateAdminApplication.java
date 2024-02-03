4d6748f7cb8629ecb17ccff3c22f504f3b6ec78b
hahaharin <harin2950@naver.com>
2024-02-03 22:42:05 +0900

    [feat] 관리자 서버 세팅

---
+++ src/main/java/hello/realestateadmin/RealEstateAdminApplication.java
@@ -0,0 +1,13 @@
+package hello.realestateadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/realestateadmin/RealEstateAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.realestateadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
